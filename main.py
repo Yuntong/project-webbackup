1e940a3e0245bfddcbb764b53465f1e03af752a1
Firesnail <yuntongfuwa2008@163.com>
2015-07-20 11:43:53 +0800

    demo1

--- main.py
+++ main.py
@@ -7,17 +7,21 @@ python main.py -d jumptime -u weburl -o backpOutPath
 __author__ = 'sp'
 
 import os
+import sys
 import urllib2
 import datetime
 import re
 import chardet
+import argparse
+import time
+import logging
 
 jumptime = 60
 
-inputurl = 'http://m.sohu.com/'
-dirpath = '/tmp/backup/' # 待加入尾号有无/判断
-now = datetime.datetime.now().strftime('%Y%m%d%H%M')
-filepath = dirpath + str(now)
+# inputurl = 'http://m.sohu.com/'
+# dirpath = '/tmp/backup/' # 待加入尾号有无/判断
+# now = datetime.datetime.now().strftime('%Y%m%d%H%M')
+# filepath = dirpath + str(now)
 binUrlList = []
 textUrlList = []
 
@@ -57,10 +61,10 @@ def scrapData(rawurl):
         reponse = urllib2.urlopen(request, timeout = 60).read()
         encoding_dict = chardet.detect(reponse)
         web_encoding = encoding_dict['encoding']
-        if web_encoding == 'utf-8' or web_encoding == 'UTF-8':
-            pass
-        else:
-            reponse = reponse.decode('gbk','ignore').encode('utf-8')
+        # if web_encoding == 'utf-8' or web_encoding == 'UTF-8':
+        #     pass
+        # else:
+        #     reponse = reponse.decode('gbk','ignore').encode('utf-8')
 
     except urllib2.HTTPError,e:
         print(e.reason)
@@ -75,33 +79,62 @@ def saveFile(data,path,method):
     f.write(data)
     f.close()
 
+def main(arg):
+    one = 1
+    hp = argparse.ArgumentParser()
+    hp.add_argument("-d", "--jumptime", required = True,
+                help = "时间间隔")
+    hp.add_argument("-u", "--url", required = True,
+                help = "备份网址")
+    hp.add_argument("-o", "--output", required = True,
+                help = "备份路径")
+    args = vars(hp.parse_args())
+
+    while 1:
+        now = datetime.datetime.now().strftime('%Y%m%d%H%M')
+        filepath = args["output"] + str(now)
+        creatFolder(filepath)
+        htmltext = scrapData(args["url"])
+        textLinkPre = r'http://.*?\.js|' \
+                      r'http://.*?\.css'
+
+        binLinkPre = r'http://.*?\.jpg|' \
+                     r'http://.*?\.png|' \
+                     r'http://.*?\.gif|' \
+                     r'http://.*?\.ico|' \
+                     r'http://.*?\.svg'
+        textUrlList = parse(htmltext, textLinkPre)
+        binUrlList = parse(htmltext, binLinkPre)
+
+
+        saveFile(htmltext,filepath+'/index.html','w')
+
+        # 资源链接转换为本地 待解决
+        # Save textdata by textUrlList
+        for t in range(len(textUrlList)):
+            text = scrapData(textUrlList[t])
+            namepre = r'(?<=/)[^/]*(?=[\n\r]|$)'
+            name = parse(textUrlList[t], namepre)
+            saveFile(text,filepath+'/'+name[0],'w')
+
+        # Save binarydata by binUrlList
+        for p in range(len(binUrlList)):
+            data = scrapData(binUrlList[p])
+            namepre = r'(?<=/)[^/]*(?=[\n\r]|$)'
+            name = parse(binUrlList[p], namepre)
+            saveFile(data, filepath+'/'+name[0],'wb')
+
+        print one
+        one = one + 1
+        time.sleep(int(args["jumptime"]))
+
 if __name__ == '__main__':
-    creatFolder(filepath)
-    htmltext = scrapData(inputurl)
-    textLinkPre = r'http://.*?\.js|' \
-                  r'http://.*?\.css'
+    arg = sys.argv
+    main(arg)
 
-    binLinkPre = r'http://.*?\.jpg|' \
-                 r'http://.*?\.png|' \
-                 r'http://.*?\.gif|' \
-                 r'http://.*?\.ico|' \
-                 r'http://.*?\.svg'
 
-    textUrlList = parse(htmltext, textLinkPre)
-    binUrlList = parse(htmltext, binLinkPre)
 
-    saveFile(htmltext,filepath+'/index.html','w')
-    # 资源链接转换为本地 待解决
-    # Save textdata by textUrlList
-    for t in range(len(textUrlList)):
-        text = scrapData(textUrlList[t])
-        namepre = r'(?<=/)[^/]*(?=[\n\r]|$)'
-        name = parse(textUrlList[t], namepre)
-        saveFile(text,filepath+'/'+name[0],'w')
 
-    # Save binarydata by binUrlList
-    for p in range(len(binUrlList)):
-        data = scrapData(binUrlList[p])
-        namepre = r'(?<=/)[^/]*(?=[\n\r]|$)'
-        name = parse(binUrlList[p], namepre)
-        saveFile(data, filepath+'/'+name[0],'wb')
+    
+
+
